73ee9cdaa9e28f85b6300a4546d518b79891f46a
Shauryaarajput <planixos8@gmail.com>
2026-03-06 15:40:59 +0530

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import MainLayout from "./component/mainlayout";
+import MainLayout from "./component/MainLayout";
 import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
 import AdminHome from "./pages/AdminDashboard/AdminHome";
 import About from "./pages/navbar/About";
